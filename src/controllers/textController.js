5b52f6c149cbfc5f9af835ecca87520785a4748f
Adnanrobi <adnanrobi6@gmail.com>
2024-05-09 14:18:03 +0600

    - created controllers for text operations

---
+++ src/controllers/textController.js
@@ -0,0 +1,45 @@
+const Text = require("../models/Text");
+const { analyzeText } = require("../services/textService");
+
+const createText = async (req, res) => {
+  try {
+    const { content } = req.body;
+    const newText = new Text({ content });
+    await newText.save();
+    res.json(newText);
+  } catch (err) {
+    res.status(500).send("Server Error");
+  }
+};
+
+const getTextStats = async (req, res) => {
+  try {
+    const text = await Text.findById(req.params.id);
+    if (!text) {
+      return res.status(404).json({ msg: "Text not found" });
+    }
+    const stats = analyzeText(text.content);
+    res.json(stats);
+  } catch (err) {
+    res.status(500).send("Server Error");
+  }
+};
+
+const getLongestWords = async (req, res) => {
+  try {
+    const text = await Text.findById(req.params.id);
+    if (!text) {
+      return res.status(404).json({ msg: "Text not found" });
+    }
+    const longestWords = analyzeText(text.content).longestWords;
+    res.json(longestWords);
+  } catch (err) {
+    res.status(500).send("Server Error");
+  }
+};
+
+module.exports = {
+  createText,
+  getTextStats,
+  getLongestWords,
+};
